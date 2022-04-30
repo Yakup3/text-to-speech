a62dc600dad7064dee7e40817e669b0c9f256ba6
unknown <yakupmalikov@gmail.com>
2022-04-30 23:44:19 +0300

    text to speech using react js

---
+++ src/components/Home.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./Home.css";
+import { useSpeechSynthesis } from "react-speech-kit";
+import { localStrings } from "../shared/shared.constants";
+
+const Home = () => {
+  const [text, setText] = useState("");
+  const [alert, setAlert] = useState(false);
+  const { speak, cancel } = useSpeechSynthesis();
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleOnButtonClick = () => {
+    text.trim().length == 0 ? setAlert(true) : setAlert(false);
+
+    speak({ text: text });
+  };
+
+  const handleOnCancelClick = () => {
+    cancel();
+  };
+
+  const renderHeader = () => {
+    return <h2>{localStrings.header}</h2>;
+  };
+
+  const renderForm = () => {
+    return (
+      <form>
+        <textarea
+          placeholder={localStrings.placeholder}
+          required
+          onChange={handleOnChange}
+        ></textarea>
+      </form>
+    );
+  };
+
+  const renderAlert = () => {
+    return <p>{localStrings.alert}</p>;
+  };
+
+  const renderButtons = () => {
+    return (
+      <div className="buttons">
+        <button className="speech" onClick={handleOnButtonClick}>
+          {localStrings.speech}
+        </button>
+        <button className="cancel" onClick={handleOnCancelClick}>
+          {localStrings.cancel}
+        </button>
+      </div>
+    );
+  };
+
+  return (
+    <div className="container">
+      {renderHeader()}
+      {renderForm()}
+      {alert && renderAlert()}
+      {renderButtons()}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/shared/shared.constants.js
@@ -0,0 +1,7 @@
+export const localStrings = {
+  header: "Text to Speech",
+  speech: "Speak",
+  cancel: "Stop",
+  placeholder: "Enter your text",
+  alert: "Please type something!!!",
+};
